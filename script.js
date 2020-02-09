2d59479585128214e97eb7401024d9066be2a0fb
Quindallyn <qrk5002@gmail.com>
2020-02-09 00:10:09 -0500

    replicated example in script and html

--- script.js
+++ script.js
@@ -1,23 +1,80 @@
-fetch('http://localhost:3000/notes/', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ title: 'Hi', body: 'COOL' })
-})
-  .then(r => r.json())
-  .then()
+function print (value) {
+  console.log(value)
+  return value
+}
 
-fetch('http://localhost:3000/notes/:id', {
-  method: 'PATCH',
-  headers: { 'Conent-Type': 'application/json' },
-  body: JSON.stringify({ title: 'Hi', body: 'COOL' })
-})
-  .then(r => r.json())
-  .then()
+function q (selector) {
+  return document.querySelector(selector)
+}
 
-fetch('http://localhost:3000/notes/:id', {
-  method: 'DELETE',
-  headers: { 'Conent-Type': 'application/json' },
-  body: JSON.stringify({ title: 'Hi', body: 'COOL' })
+function getAllNotes () {
+  return fetch('http:localhost:3000/notes/', {
+    method: 'GET'
+  })
+    .then(r => r.json())
+}
+
+function createNotesHTML (notes) {
+  let notesStr = '<ul id="note-list">'
+  for (const note of notes) {
+    notesStr += createNoteHTML(note)
+  }
+  notesStr += '</ul>'
+  return notesStr
+}
+
+function createNoteHTML (note) {
+  return `<li data-note-id="${note.id}">${note.note} <button class="delete">Delete</button></li>`
+}
+
+function postNewNote (noteText) {
+  return fetch('http://localhost:3000/notes/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ note: noteText, done: false, created: moment().format() })
+  })
+    .then(r => r.json())
+}
+function renderNotesList (notes) {
+  const notesHTML = createNotesHTML(notes)
+  const notesSection = document.querySelector('#notes')
+  notesSection.innerHTML = notesHTML
+}
+
+function renderNewNote (note) {
+  const noteHTML = createNoteHTML(note)
+  const notesList = document.querySelector('#notes-list')
+  notesList.insertAdjacentHTML('beforeend', noteHTML)
+}
+
+getAllNotes().then(renderNotesList)
+
+q('#new-note-form').addEventListener('submit', event => {
+  event.preventDefault()
+  const noteTextField = q('#note-text')
+  const noteText = noteTextField.value
+  noteTextField.value = ''
+  postNewNote(noteText).then(renderNewNote)
 })
-  .then(r => r.json())
-  .then()
+
+q('#notes').addEventListener('click', event => {
+  if (event.target.matches('.delete')) {
+    print('delete ' + event.target.parentElement.dataset.noteId)
+  }
+})
+
+// fetch('http://localhost:3000/notes/:id', {
+//   method: 'PATCH',
+//   headers: { 'Conent-Type': 'application/json' },
+//   body: JSON.stringify({ title: 'Hi', body: 'COOL' })
+// })
+//   .then(r => r.json())
+//   .then()
+
+// fetch('http://localhost:3000/notes/:id', {
+//   method: 'DELETE',
+//   headers: { 'Conent-Type': 'application/json' },
+//   body: JSON.stringify({ title: 'Hi', body: 'COOL' })
+// })
+//   .then(r => r.json())
+//   .then()
